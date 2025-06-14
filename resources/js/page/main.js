30197fc16877047d752bd50f51c130a979bab990
김진환 <kimjinhwan1897@gmail.com>
2025-06-15 01:35:22 +0900

    auto commit

---
+++ resources/js/page/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	
+	//쿠키 정보 확인	
+    var intro = getCookie('intro');
+    var noticeBar = getCookie('noticeBar');
+    
+    //false 인경우
+    if (!noticeBar){
+	
+	    var todayDate = new Date(); 
+	    todayDate = new Date(parseInt(todayDate.getTime() / 86400000) * 86400000 + 54000000);
+		
+	    if (todayDate > new Date()) {
+	    	deleteCookie('noticeBar');
+	    }
+	    
+    }
+	
+    
+    // 변수가 없을경우 인트로 출력 출력 
+    if (!intro) {
+		
+	    var todayDate = new Date(); 
+	    todayDate = new Date(parseInt(todayDate.getTime() / 86400000) * 86400000 + 54000000);
+		//alert('introFn > ' + todayDate + " vs " + new Date());
+
+	    //if (todayDate > new Date()) {
+			//alert('introFn');
+			/*개발종료 후 다시 열기*/
+			//introFn();
+	    //}
+	    
+    	setCookie00('intro', 'done',1);    	
+    }
+    // 변수가 있을 경우 인트로 숨김
+    else{
+    	$("#intro").hide();
+    	//setCookie00('intro', 'done',1);    	
+    }
+    
+});

b7cd5d129e0ec66f765423ce45176958aee5e46d
김진환 <kimjinhwan1897@gmail.com>
2025-06-21 15:05:18 +0900

    auto commit

--- resources/js/page/main.js
+++ resources/js/page/main.js
@@ -19,28 +19,29 @@ $(document).ready(function() {
 	    
     }
 	
+	introFn();
     // 변수가 없을경우 인트로 출력 출력 
-    if (!intro) {
+    // if (!intro) {
 		
-	    var todayDate = new Date(); 
-	    todayDate = new Date(parseInt(todayDate.getTime() / 86400000) * 86400000 + 54000000);
-		//alert('introFn > ' + todayDate + " vs " + new Date());
+	//     var todayDate = new Date(); 
+	//     todayDate = new Date(parseInt(todayDate.getTime() / 86400000) * 86400000 + 54000000);
+	// 	//alert('introFn > ' + todayDate + " vs " + new Date());
 
-	    //if (todayDate > new Date()) {
-			//alert('introFn');
-			/*개발종료 후 다시 열기*/
+	//     //if (todayDate > new Date()) {
+	// 		//alert('introFn');
+	// 		/*개발종료 후 다시 열기*/
 
-			//alert('introFn');
-			introFn();
-	    //}
+	// 		//alert('introFn');
+	// 		introFn();
+	//     //}
 	    
-    	setCookie00('intro', 'done',1);    	
-    }
-    // 변수가 있을 경우 인트로 숨김
-    else{
-    	$("#intro").hide();
-    	//setCookie00('intro', 'done',1);    	
-    }
+    // 	setCookie00('intro', 'done',1);    	
+    // }
+    // // 변수가 있을 경우 인트로 숨김
+    // else{
+    // 	$("#intro").hide();
+    // 	//setCookie00('intro', 'done',1);    	
+    // }
      
     
 });
